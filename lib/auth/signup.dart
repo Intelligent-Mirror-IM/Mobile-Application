588bae2c613f5835f064ab2146290ee8be60e35d
Majdoleenshaderma <158805275+Majdoleenshaderma@users.noreply.github.com>
2025-03-13 18:02:53 +0300

    Update signup.dart

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -11,22 +11,121 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Create account",style: TextStyle(color: Colors.white),),
-        iconTheme: const IconThemeData(color: Colors.white),
-        backgroundColor: Colors.purple.shade900,
-      ),
-      body: ListView(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(40),
-                child: Image.asset("images/102.png"),
-              )
-            ],
-          )
-        ],
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40),
+              child: Center(child: Image.asset("images/102.png")),
+            ),
+            Text(
+              "Sign Up",
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.purple.shade900,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      cursorColor: Colors.indigo.shade900,
+                      decoration: const InputDecoration(
+                        hintText: "Email",
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      cursorColor: Colors.indigo.shade900,
+                      decoration: const InputDecoration(
+                        hintText: "Username",
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      obscureText: true,
+                      cursorColor: Colors.purple.shade900,
+                      decoration: const InputDecoration(
+                        hintText: "Password",
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      obscureText: true,
+                      cursorColor: Colors.purple.shade900,
+                      decoration: const InputDecoration(
+                        hintText: "Confirm password",
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+                      child: Row(
+                        children: [
+                          const Text(
+                            "Have An Account ?  ",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.of(context).pushNamed("Login");
+                            },
+                            child: Text(
+                              "LOGIN",
+                              style: TextStyle(
+                                  color: Colors.indigo.shade900,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple.shade900,
+                        minimumSize: const Size(180, 50),
+                      ),
+                      child: const Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontSize: 23,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
